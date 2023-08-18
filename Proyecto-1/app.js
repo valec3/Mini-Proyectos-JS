7120884c6acc8a29baba4549ea52b81787aebd8a
Victor Raul <v1c70rotakmer@gmail.com>
2023-08-18 11:31:49 -0500

    Agregar componente para errores

--- Proyecto-1/app.js
+++ Proyecto-1/app.js
@@ -8,8 +8,14 @@ const humedad = document.querySelector(".humedad");
 const viento = document.querySelector(".viento");
 const btnSearch = document.querySelector("#btnSearch");
 const search = document.querySelector("#searchCity");
+const btnError = document.querySelector(".error a");
 
+btnError.addEventListener("click", () =>document.querySelector(".error").classList.remove("active"));
 btnSearch.addEventListener("click", getValueSearch);
+search.addEventListener("keydown", event => {
+    if (event.key === "Enter") {
+        getValueSearch();    }
+});
 
 function getValueSearch() {
     const ciudad = search.value;
@@ -19,18 +25,28 @@ function changeImgWeather(weather) {
     const imgClima = document.querySelector("#state-clima");
     imgClima.src = `./img/${weather}.png`;
 }
-async function changeCityWeather(city) {
-    const data = await getWeatherData(city);
-    titleCity.textContent = data.name;
-    temp.textContent = Math.round(data.main.temp)+"°C";
-    humedad.textContent = data.main.humidity+"%";
-    viento.textContent = data.wind.speed+"Km/h";
-    changeImgWeather(data.weather[0].main);
+async function changeCityWeather(city="lima") {
+        const data = await getWeatherData(city);
+        console.log(data);
+        if (data === null) return;
+        titleCity.textContent = data.name;
+        temp.textContent = Math.round(data.main.temp)+"°C";
+        humedad.textContent = data.main.humidity+"%";
+        viento.textContent = data.wind.speed+"Km/h";
+        changeImgWeather(data.weather[0].main);
 }
 
 async function getWeatherData(city="lima") {
-    const response = await fetch(API_URL+`&q=${city}`);
-    const data = await response.json();
-    console.log(data);
-    return data;
+        try{
+            const response = await fetch(API_URL+`&q=${city}`);
+            const data = await response.json();
+            if (!response.ok) {
+                document.querySelector(".error").classList.add("active");
+                return null;
+            }
+            return data;
+        }catch(error){
+            console.log(error);
+            return null;
+        }
 }
